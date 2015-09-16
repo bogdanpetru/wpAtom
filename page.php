693450b62e68fc968beed24d7112cf3a18a1111e
Petru <bogdanp.netlogiq@gmail.com>
2015-09-16 10:45:33 +0300

    more content

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Template Name: Name of themplate
+ * @name Simple Page Template
+ * @package wpApp
+ * @author: Bogdan Petru Pintican
+ */
+global $wpApp;
+the_post();
+?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+		<div class="row">
+			
+			<main id="main col-sm-8">
+				<?php the_content(); ?>
+			</main>
+			<!-- main -->
+
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+
+		</div>
+		<!-- .row -->
+	</div>
+	<!-- .container -->
+
+<?php get_footer(); ?>
